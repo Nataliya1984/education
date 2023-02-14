904c80b4d3ed7199e1095b87de6b936124073cbf
Natalya1984 <lisa.volma1984@gmail.com>
2023-02-15 02:09:05 +0300

    get post

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {v1} from 'uuid';
 import './styles/App.css'
 import {PostList} from "./components/PostList";
@@ -6,12 +6,19 @@ import {PostForm} from "./components/PostForm";
 import {PostFilter} from './components/UI/PostFilter';
 import {MyModal} from "./components/UI/Modal/MyModal";
 import {MyButton} from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePost";
+import PostService from "./API/PostService";
+import {Loader} from "./components/UI/Loader/Loader";
 
 
 export type PostType = {
-    id: string
-    title: string
-    body: string
+    // id: string
+    // title: string
+    // body: string
+    "userId": number
+    "id": number
+    "title": string
+    "body": string
 }
 
 export type FilteredPostType = {
@@ -19,39 +26,64 @@ export type FilteredPostType = {
     query: string
 }
 
+export type GetPostResponseType = {
+    "userId": number
+    "id": number
+    "title": string
+    "body": string
+}
+
 function App() {
     const [post, setPost] = useState<Array<PostType>>([
-        {id: v1(), title: 'JavaScript', body: 'Description'},
-        {id: v1(), title: 'React', body: 'Ttttttttttt'},
-        {id: v1(), title: 'CSS', body: 'Vppppp'},
+        // {id: v1(), title: 'JavaScript', body: 'Description'},
+        // {id: v1(), title: 'React', body: 'Ttttttttttt'},
+        // {id: v1(), title: 'CSS', body: 'Vppppp'},
     ])
 
 
     const [filter, setFilter] = useState<FilteredPostType>({sort: '', query: ''})
 
-    const [modal, setModal]=useState<boolean>(false)
+    const [modal, setModal] = useState<boolean>(false)
 
     //для того, что бы сделать поиск необходимо сделать фильтрацию и удалять ненужные элементы из массива
     // const sortedPost =getSortedPost()
 
-    const sortedPost = useMemo(() => {
-        console.log('функция сорт отработала')
-        if (filter.sort) {
-            return [...post.sort((a, b) => a > b ? 1 : -1)]
-        } else {
-            return post
-        }
-    }, [filter.sort, post])
+    //переносим в папку hooks
+    // const sortedPost = useMemo(() => {
+    //     console.log('функция сорт отработала')
+    //     if (filter.sort) {
+    //         return [...post.sort((a, b) => a > b ? 1 : -1)]
+    //     } else {
+    //         return post
+    //     }
+    // }, [filter.sort, post])
 
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPost])
+    //переносим в папку hooks
+    // const sortedAndSearchedPosts = useMemo(() => {
+    //     return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query))
+    // }, [filter.query, sortedPost])
+
+    const sortedAndSearchedPosts = usePosts(post, filter.sort, filter.query)
+    const [isPostLoading, setPostLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
 
     const createPost = (newPost: any) => {
         setPost([...post, newPost])
         // после создания поста, модалка скрывается
         setModal(false)
+    }
+
+    //функция которая отправляет запрос на сервер, получает какие-то данные и помещает в наше состояние с постами
+    async function fetchPosts() {
+        setPostLoading(true)
+        //создаем переменную куда будет помещен результат выполнения запроса
+        const posts = await PostService.getAll();
+        setPost(posts)
+        setPostLoading(false)
     }
 
     const removePost = (posts: PostType) => {
@@ -69,7 +101,7 @@ function App() {
 
     return (
         <div className="App">
-            <MyButton style={{marginTop:'30px'}} onClick={()=>setModal(true)}>
+            <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
                 Создать пост
             </MyButton>
             <MyModal visible={modal} setVisible={setModal}>
@@ -79,11 +111,16 @@ function App() {
             <hr style={{margin: '15px 0'}}/>
             {/*реализовываем поиск, находит нужный пост, остальные исчезают, добавим placheholder и сделаем инпут управляемым*/}
             <PostFilter filter={filter} setFilter={setFilter}/>
+            {isPostLoading
+                ? <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}}>
+                    <Loader/>
+                </div>
+                : <PostList post={sortedAndSearchedPosts}
+                            title={'Посты про JS'}
+                            removePost={removePost}
+                />
+            }
 
-            <PostList post={sortedAndSearchedPosts}
-                      title={'Список постов 1'}
-                      removePost={removePost}
-            />
         </div>
     );
 }
---
+++ src/hooks/usePost.ts
@@ -0,0 +1,29 @@
+import {useMemo} from "react";
+import {PostType} from "../App";
+
+
+//хук который занимается сортировкой
+export const useSortedPosts = (post:Array<PostType>, sort:string) => {
+    const sortedPost = useMemo(() => {
+       // console.log('функция сорт отработала')
+        if (sort) {
+            return [...post.sort((a, b) => a > b ? 1 : -1)]
+        } else {
+            return post
+        }
+    }, [sort, post])
+    return sortedPost
+}
+
+
+//хук который будет возвращать уже и отфильтрованный и отсортированный массив
+export const usePosts = (posts:Array<PostType>, sort:string, query:string) => {
+
+    //необходимо получить массив отсортированных постов, для этого у нас есть хук в него передаем посты и передаем метод сортировки
+    const sortedPost =useSortedPosts(posts, sort)
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPost.filter(post => post.title.toLowerCase().includes(query))
+    }, [query, sortedPost])
+
+    return sortedAndSearchedPosts
+}
