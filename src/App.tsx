fb0221d52442a9bf427e78a46c97669665b207c6
Natalya1984 <lisa.volma1984@gmail.com>
2023-02-16 23:22:10 +0300

    route_3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './styles/App.css'
-import {BrowserRouter, Link, Navigate, Route, Routes} from "react-router-dom";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import {About} from "./pages/About";
 import Posts from "./pages/Posts";
 import {Navbar} from "./components/UI/Navbar/Navbar";
@@ -8,20 +8,20 @@ import {ErrorPage} from "./pages/ErrorPage";
 
 
 function App() {
-return(
-    <BrowserRouter>
-        <Navbar/>
-        <Routes>
-            <Route path='/' element={<Posts/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path="404" element={
-                <ErrorPage/>
-            }
-            />
-            <Route path="*" element={<Navigate to='404'/>}/>
-        </Routes>
-    </BrowserRouter>
-)
+    return (
+        <HashRouter>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Posts/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path="/404" element={
+                    <ErrorPage/>
+                }
+                />
+                <Route path="*" element={<Navigate to='/404'/>}/>
+            </Routes>
+        </HashRouter>
+    )
 
 }
 
--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 export const Navbar = () => {
     return (
         <div className={'navbar'}>
             <div className="navbar__links">
-                <Link to="/about" style={{textDecoration:'none'}}> О сайте </Link>
-                <Link to="/" style={{textDecoration:'none'}}>Посты</Link>
+                <NavLink to='/' style={{textDecoration:'none'}}>Посты</NavLink>
+                <NavLink to="/about" style={{textDecoration:'none'}}> О сайте </NavLink>
             </div>
         </div>
     );
